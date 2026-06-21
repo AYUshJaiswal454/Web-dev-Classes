415c7fe40435721304429d81f9ae1d982bff93bb
Ayush Jaiswal <kumarayush08757@gmail.com>
2026-06-21 13:47:37 +0530

    lecture 5

---
+++ Lecture 5/server2.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const app=express();
+
+
+
+app.use((req,res,next)=>{
+    console.log("middleware 1.. user trying to login");
+    next();
+    
+})
+
+
+app.use((req,res,next)=>{
+    console.log(req.url);
+    console.log(req.ip);
+    let auth=false;
+    if(auth){
+        next();
+    }
+    else{
+        console.log("something is wrong");
+        
+    }
+    
+    
+});
+
+app.get("/",(req,res)=>{
+    console.log(req.url);
+    console.log(req.ip);
+    res.send("home page...")
+    
+    
+})
+
+app.get("/payment",(req,res)=>{
+    res.send("payment page..")
+})
+
+app.listen(3000,()=>{
+    console.log(`server is running at 3000`);
+    
+});
---
+++ Lecture 5/server3.js
@@ -0,0 +1,47 @@
+const express=require("express");
+const app=express();
+const data=require("./data.json");
+console.log(data);
+
+
+
+
+
+app.use((req,res,next)=>{
+    console.log("middleware 1.. user trying to login");
+    next();
+    
+})
+
+
+app.use((req,res,next)=>{
+    console.log(req.url);
+    console.log(req.ip);
+    let auth=false;
+    if(auth){
+        next();
+    }
+    else{
+        console.log("something is wrong");
+        
+    }
+    
+    
+});
+
+app.get("/",(req,res)=>{
+    console.log(req.url);
+    console.log(req.ip);
+    res.send(data);
+    
+    
+})
+
+app.get("/payment",(req,res)=>{
+    res.send("payment page..")
+})
+
+app.listen(3000,()=>{
+    console.log(`server is running at 3000`);
+    
+});
